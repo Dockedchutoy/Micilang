f9b944ce7f4df8fc01cd075dd81dcb72c9e9cf89
Dockedchutoy <dockedchutoy@gmail.com>
2025-12-02 14:22:10 +0100

    parser dodod count.mcl changed

--- interpreter.py
+++ interpreter.py
@@ -13,7 +13,7 @@ TODO:
 
 Grammer:
 
-statement -> term SEMICOLON;
+expression -> term SEMICOLON;
 term -> factor ( ( "-" | "+" ) factor )* ;
 factor -> primary ( ( "/" | "*" ) primary )* ;
 primary -> NUMBER | STRING | "true" | "false" | "null" | "(" expression ")" ;
@@ -23,7 +23,9 @@ operation -> statement operator statement
 
 # Získání kódu
 
-user_code = 'var x = 2 + 1; // This is a comment\nwrite(x);'      # Tady půjde Micilang kód. Soubory a shell vymyslím později.
+user_code = 'var x = 1 + 6 / 2; // This is a comment\nwrite(x);'      # Tady půjde Micilang kód. Soubory a shell vymyslím později.
+
+user_code = "1 * 2 + 6 / 2; // 5"
 
 # Queue object
 
@@ -43,8 +45,9 @@ class Lexer():
 
         self.code = code
 
-        self.KEYWORDS = ["VAR",          # Statementy
-                "WRITE", "INPUT"]        # Funkce
+        self.KEYWORDS = ["VAR", "PRINT",         # Statementy
+                "INPUT"        # Funkce
+                ]                       # Další speciální
 
         self.tokens = [] # Veškeré tokeny
         self.chars = "" # momentální paměť
@@ -90,7 +93,7 @@ class Lexer():
                 print(self.tokens)
     
             elif self.code[self.cur] == ";":          # Konec statementu
-                self.tokens.append(("SEMICOLON"))
+                self.tokens.append(("SEMICOLON", None))
                 print(self.tokens)
                 self.cur += 1
             
@@ -99,30 +102,32 @@ class Lexer():
                     self.cur += 2
                     while self.cur < len(self.code) and self.code[self.cur] != "\n": self.cur += 1
                 else: #NEZAPOMENOUT TO UDĚLAT I PRO /// !!!!!!
-                    self.tokens.append(("SLASH"))
+                    self.tokens.append(("SLASH", None))
                     print(self.tokens)
                     self.cur += 1
             
             elif self.code[self.cur] == "*":
-                self.tokens.append(("STAR"))
+                self.tokens.append(("STAR", None))
+                print(self.tokens)
+                self.cur += 1
     
             elif self.code[self.cur] == "=":
-                self.tokens.append(("EQUAL"))
+                self.tokens.append(("EQUAL", None))
                 print(self.tokens)
                 self.cur += 1
             
             elif self.code[self.cur] == "+":
-                self.tokens.append(("PLUS"))
+                self.tokens.append(("PLUS", None))
                 print(self.tokens)
                 self.cur += 1
         
             elif self.code[self.cur] == "(":
-                self.tokens.append(("L_PARENS"))
+                self.tokens.append(("L_PARENS", None))
                 print(self.tokens)
                 self.cur += 1
 
             elif self.code[self.cur] == ")":
-                self.tokens.append(("R_PARENS"))
+                self.tokens.append(("R_PARENS", None))
                 print(self.tokens)
                 self.cur += 1
         
@@ -146,21 +151,72 @@ class Parser():
         self.tokens = tokens
         self.cur = 0
     
-    # def check(self, type):
-    #     if self.cur
+    def peek(self): # Získá současný token
+        return self.tokens[self.cur]
+
+    def previous(self): # Získá předchozí token
+        return self.tokens[self.cur - 1]
     
-    def match(self, *types):
-        if self.tokens[self.cur] in types:
-            pass
+    def advance(self):  # Přesune se o další pozici
+        if self.peek()[0] != "EOF":
+            self.cur += 1
+        return self.previous()
     
-    def statement(self):
+    def check(self, type):  # Shoduje se další token s tím co chceme?
+        if self.peek()[0] == "EOF": 
+            return False
+        return self.peek()[0] == type
+    
+    def match(self, *types):     # Pokud je další token to co chceme, pokračujeme 
+        for type in types:
+            if self.check(type):
+                self.advance()
+                return True
+        return False
+
+    def expect(self, type, message):     # Pokud není další token to co chceme, máme problém
+        if self.check(type):
+            return self.advance()
+        
+        # error handling later
+    
+    def expression(self): # expression -> term SEMICOLON;
         return self.term()
+    
+        # semicolons later
 
-    def term(self):
-        stat = self.factor()
-
+    def term(self): # term -> factor ( ( "-" | "+" ) factor )* ;
+        expr = self.factor()
         while self.match("PLUS", "MINUS"):
-            pass
+            op = self.previous()
+            right = self.factor()
+            expr = ("binary", expr, op, right)
+        return expr
+
+    def factor(self): # factor -> primary ( ( "/" | "*" ) primary )* ;
+        expr = self.primary()
+        while self.match("STAR", "SLASH"):
+            op = self.previous()
+            right = self.primary()
+            expr = ("binary", expr, op, right)
+        return expr
+
+    def primary(self): # primary -> NUMBER | STRING | "true" | "false" | "null" | "(" expression ")" ;
+        if self.match("STRING"):
+            return ("string", self.previous()[1])
+    
+        elif self.match("NUMBER"):
+            return ("number", int(self.previous()[1]))
+
+        elif self.match("L_PARENS"):
+            expr = self.expression()
+            self.expect("R_PARENS", "Missing \")\" after expression.")
+            return ("grouping", expr)
+        
+        # error handling here
+    
+    def parse(self):    # Hlavní funkce
+        return self.expression()
 
 # Hl. loop
 
@@ -168,3 +224,7 @@ if __name__ == "__main__":
     lexer = Lexer(user_code)
     lexer_out = (lexer.gettokens())
     print(lexer_out)
+
+    parser = Parser(lexer_out)
+    parser_out = parser.parse()
+    print(parser_out)
